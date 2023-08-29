e0ebe43b7a66ac80de7340430c3d7ed92da112cb
Ivan Volkov <volkivanv@mail.ru>
2023-08-29 22:17:42 +0300

    Добавлен условный оператор

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите имя пользователя!");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Masha"){
+    Console.WriteLine("Hurra, this is Masha!");
+
+} else {
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
