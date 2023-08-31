f7b5637eaaf7977f0c0bb9604da471fc60009b68
Ivan Volkov <volkivanv@mail.ru>
2023-08-31 22:11:56 +0300

    Добавил задачу с собакой

---
+++ TwoFriendsAndDog/Program.cs
@@ -0,0 +1,19 @@
+﻿int count = 0;
+double distance = 10000;
+double firstFriendSpeed = 2;
+double secondFriendSpeed = 5;
+double dogSpeed = 50;
+double friend = 2;
+double time;
+while(distance > 10){
+    if(friend == 1){
+        time = distance/(dogSpeed + secondFriendSpeed);
+        friend = 2;
+    } else {
+        time = distance/(secondFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+    distance -= time*(firstFriendSpeed + secondFriendSpeed);
+    count++;
+}
+Console.WriteLine("Количество пробегов собаки " + count);
