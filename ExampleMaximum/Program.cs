ac649853c334fe21c2fa50f08ccc54df01b40eeb
Ivan Volkov <volkivanv@mail.ru>
2023-09-19 19:54:08 +0300

    Добавлены матрицы и рекурсии

---
+++ ExampleMaximum/Program.cs
@@ -0,0 +1,27 @@
+﻿
+//int[] inArray = new int[N]
+int maximum(int[] inArray){
+    int n = inArray.Length;
+    int max = inArray[0];
+    for(int i = 1; i < n; i++){
+        if (inArray[i] > max){
+            max = inArray[i];
+        }
+    }
+    return max;
+}
+
+Console.WriteLine("Input length of array");
+int N = int.Parse(Console.ReadLine());
+
+int[] Numbers = new int[N];
+
+for(int i = 0; i < N; i++){
+    Numbers[i] = new Random().Next(0,30);
+}
+for(int i = 0; i < N; i++){
+    Console.Write(Numbers[i]+" ");
+}
+Console.WriteLine("");
+Console.WriteLine(maximum(Numbers));
+
---
+++ Exapmple014_Factorial/Program.cs
@@ -0,0 +1,25 @@
+﻿int ReadData(string msg) // вводим данные
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+
+long FactorialRec(int n)
+{
+    if(n == 1) return 1;
+    return n * FactorialRec(n-1);
+}
+
+long FibbonaciRec(int n)
+{
+    if(n == 0) return 0;
+    if(n == 1) return 1;
+    return FibbonaciRec(n-1)+FibbonaciRec(n-2);
+}
+
+int n = ReadData("Input N");
+Console.WriteLine($"Факториал числа {n} равен {FactorialRec(n)}.");
+Console.WriteLine($"Число Фиббоначи от {n} равно {FibbonaciRec(n)}.");
+
+
